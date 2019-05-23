b1f8db92b1e87e7c4617f320911d3974c8d2e459
df424 <d.l.flanagan01@gmail.com>
2019-05-22 21:56:17 -0400

    Moved keras tutorial into seperate notebook.

--- learning/keras_basic_classification.py
+++ learning/keras_basic_classification.py
@@ -1,37 +1,14 @@
 
-# This file follows the tutorial from https://www.tensorflow.org/tutorials/keras/basic_classification as per the instructions...
+import matplotlib.pyplot as plt
 
-import tensorflow as tf
-from tensorflow import keras
-import numpy as np
-
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
-               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-
-# Load the data using keras' built in functions...
-(train_images, train_labels), (test_images, test_labels) = keras.datasets.fashion_mnist.load_data()
-
-
-def createBasicANN(input_shape, n_outputs, layers, activation):
-    _layers = [keras.layers.Flatten(input_shape=input_shape)]
-
-    for l in layers:
-        _layers.append(l, activation = activation)
-
-    _layers.append(n_outputs, activation=tf.nn.softmax)
-
-
-[[1, 0, 0, 1, 0, 0, 1, 0],
-[1, 0, 0, 1, 0, 0, 1, 1],
-[0, 1, 0, 1, 0, 0, 1, 0],
-[0, 0, 1, 0, 1, 0, 1, 0],
-[0, 0, 1, 0, 0, 1, 0, 0],
-[0, 0, 1, 0, 0, 1, 0, 1],
-[0, 1, 0, 0, 0, 1, 0, 1],
-[1, 0, 0, 0, 1, 0, 1, 0],
-[1, 0, 0, 0, 0, 1, 0, 0],
-[0, 0, 1, 0, 1, 0, 0, 0],
-[1, 0, 0, 0, 1, 0, 0, 1],
-[0, 1, 0, 0, 1, 0, 1, 1],
-[0, 1, 0, 1, 0, 0, 0, 0],
-[0, 0, 1, 0, 1, 0, 1, 1]]
+def plotHistory(history):
+    f, (ax1, ax2) = plt.subplots(1,2)
+    ax1.plot(history.history['acc'])
+    ax1.plot(history.history['val_acc'])
+    ax1.legend(['train', 'test'])
+    ax1.set_title('Accuracy')
+    ax2.plot(history.history['loss'])
+    ax2.plot(history.history['val_loss'])
+    ax2.legend(['train', 'test'])
+    ax2.set_title('Loss')
+    plt.show()
